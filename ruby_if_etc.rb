efe76ac53a8e520d8221607401a98e42f3d17e94
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-19 01:01:12 +0900

    if文

---
+++ ruby_if_etc.rb
@@ -0,0 +1,21 @@
+=begin
+nilもFalseと同じ役割をする
+
+=end
+n=11
+if n>10
+    puts "10より大きい"
+else
+    puts "10以下"
+end
+
+country="italy"
+if country =="japan"
+    puts "こんにちは"
+elsif country == "us"
+    puts "Hello"
+elsif country == "italy"
+    puts "Ciao"
+else
+    puts "???"
+end
