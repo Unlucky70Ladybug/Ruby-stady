6cc419d5b94aa971068a85763cf2855837984e2e
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-19 01:11:59 +0900

    defメゾット

--- ruby_if_etc.rb
+++ ruby_if_etc.rb
@@ -19,3 +19,15 @@ elsif country == "italy"
 else
     puts "???"
 end
+
+def greet(country)
+    #countryがnilならメッセージを返してメソッドを抜ける
+    return "countryを入力してください" if country.nil?
+    if country == "japan"
+        "こんにちは"
+    else
+        "hello"
+    end
+end
+puts greet(nil)
+puts greet("japan")
