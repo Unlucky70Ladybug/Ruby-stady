860405f9b22186a600d6faf6ad0965b62f608a9c
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-25 23:27:53 +0900

    unless

--- sample2.rb
+++ sample2.rb
@@ -8,3 +8,12 @@ a=<<"TEXT"
 ようこそ、#{name}さん。/n先日はありがとうございました。
 以下のメッセージをご覧ください。
 TEXT
+
+def ok?(status)
+  unless status == "ok"
+    return "何か異常があります"
+  else
+    return "正常です"
+  end
+end
+puts ok?("良いですか？")
