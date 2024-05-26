f219d03dcac7f91b97a6505943aa226b20eb24f4
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-27 02:03:24 +0900

    その他諸々

--- range.rb
+++ range.rb
@@ -35,3 +35,25 @@ puts liquid?(99)
 puts liquid?(100)
 
 puts (1..5).to_a
+
+jp = ["japan","日本"]
+country ="日本"
+case country
+when *jp
+  puts "こんにちは"
+end
+puts
+
+def greet(*name)
+  puts "#{name.join("と")}、こんにちは"
+end
+greet("佐藤")
+greet("佐藤","山田","鈴木")
+puts
+
+a = Array.new(10){|n| n % 3 + 1}
+puts a
+
+a = Array.new(5, "default")
+a[1] = "easy"
+puts a
