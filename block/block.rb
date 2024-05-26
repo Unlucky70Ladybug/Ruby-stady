e98d5253361adcbb5303046cfc64345e65fcfdc9
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-26 22:32:54 +0900

    ruby of block

---
+++ block/block.rb
@@ -0,0 +1,28 @@
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+puts sum
+
+a = [1,2,3,1,2,3]
+a.delete_if do |n|
+  n.odd?
+end
+puts
+puts a
+
+a = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end
+puts
+puts sum
+
+numbers = [1,2,3,4]
+sum = 0
+numbers.each{|n| sum += n}
+puts
+puts sum
