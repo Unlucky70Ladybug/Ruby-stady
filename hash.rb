ff4cdf57cbf2d7833c736a663411e4534fb47a9c
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-27 23:49:37 +0900

    convert_lengthメゾットのテスト

--- hash.rb
+++ hash.rb
@@ -15,3 +15,13 @@ currencies.each do |key_value|
   puts "#{key}:#{value}"
 end
 puts
+
+person = {
+  name: "Alice",
+  age: 20,
+  friends:["Bob", "Carol"],
+  phones:{home:"1234-0000",mobile:"5678-0000"}
+}
+puts person[:age]
+puts person[:friends]
+puts person[:phones][:mobile]
