0a6da52048667f8ce2ea5c3590c3c1bf6daf5f91
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-28 23:14:42 +0900

    convert_hash_syntax fin

--- ruby-book/test/convert_hash_syntax_test.rb
+++ ruby-book/test/convert_hash_syntax_test.rb
@@ -3,6 +3,25 @@ require_relative "../lib/convert_hash_syntax.rb"
 
 class ConvertHashSyntax < Minitest::Test
   def test_convert_hash_syntax
-    assert_equal "{}", convert_hash_syntax("{}")
+
+    old_syntax = <<~TEXT
+      {
+        :neme => "Alice,
+        :age => 20,
+        :gender => :female
+      }
+    TEXT
+
+    expected = <<~TEXT
+      {
+        neme: "Alice,
+        age: 20,
+        gender: :female
+      }
+    TEXT
+
+    actual = convert_hash_syntax(old_syntax)
+    puts actual
+    assert_equal expected , actual
   end
 end
