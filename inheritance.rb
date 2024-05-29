c3d98369db6c8c966f3fdd0ec42be2fe99512e9c
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-29 23:36:26 +0900

    継承

---
+++ inheritance.rb
@@ -0,0 +1,29 @@
+class Product
+  attr_reader :name, :price
+
+  def initialize(name,price)
+    @name = name
+    @price = price
+  end
+end
+
+class DVD < Product
+  #nameとpriceはスーパークラスでattr_readerが設定されているので定義不要
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
+  end
+
+end
+
+product = Product.new("A great movie", 1000)
+puts product.name
+puts product.price
+puts
+
+dvd = DVD.new("A great DVD", 1000, 120)
+puts dvd.name
+puts dvd.price
+puts dvd.running_time
