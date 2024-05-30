736a3f2fc49f799cac88d0cb1abff6c5aeabb993
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-31 01:53:17 +0900

    参照の方法

--- module_namespace.rb
+++ module_namespace.rb
@@ -15,5 +15,23 @@ module Clock
   end
 end
 
+module Baseball2
+  class Second
+    def file_with_nesting
+      puts File
+    end
+  end
+end
+
+class Baseball2::Second
+  def file_without_nesting
+    puts File
+  end
+end
+
 second = Baseball::Second.new("Alice", 13)
 clock = Clock::Second.new(13)
+
+second = Baseball2::Second.new
+second.file_with_nesting
+second.file_without_nesting
