3d72f7e2d7ed4aee62ec670cead94dece31e0a90
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-30 23:05:45 +0900

    class add

--- module.rb
+++ module.rb
@@ -26,3 +26,27 @@ puts product.title
 
 user = User.new
 puts user.name
+
+class Teampo
+  include Comparable
+
+  attr_reader :bpm
+
+  def initialize(bpm)
+    @bpm = bpm
+  end
+  def <=>(other)
+    other.is_a?(Teampo) ? bpm <=> other.bpm : nill
+  end
+
+  def inspect
+    "#{bpm}bpm"
+  end
+end
+
+t_120 = Teampo.new(120)
+t_180 = Teampo.new(180)
+
+puts t_120 > t_180
+puts t_120 <= t_180
+puts t_120 == t_180
