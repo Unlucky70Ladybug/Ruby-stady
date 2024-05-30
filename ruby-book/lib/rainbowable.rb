4bfa6e67763e28dfd5fcd67676036e5edbf8a004
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-05-30 22:37:15 +0900

    refactoring

--- ruby-book/lib/rainbowable.rb
+++ ruby-book/lib/rainbowable.rb
@@ -1,15 +1,13 @@
 module Rainbowable
   def rainbow
-    str = self.to_s
-    count = 0
+
     colored_chars = []
 
-    str.each_char do |char|
+    to_s.each_char.with_index do |char, count|
       color = 31 + count % 6
       colored_chars << "\e[#{color}m#{char}"
       count +=1
     end
-    ret = colored_chars.join
-    ret + "\e[0m"
+    colored_chars.join + "\e[0m"
   end
 end
