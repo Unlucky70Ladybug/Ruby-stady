7358ef6ffa7fbbfff64737daa82ed550d5746a39
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-01 21:28:15 +0900

    retry

--- exception.rb
+++ exception.rb
@@ -26,3 +26,18 @@ rescue  NoMethodError
   puts e.backtrace
   puts "------------------------------------"
 end
+
+puts
+retry_count = 0
+begin
+  puts "処理を開始します"
+  1/0
+rescue
+  retry_count += 1
+  if retry_count <= 3
+    puts "retryします。 #{retry_count}回目"
+    retry
+  else
+    puts "retryに失敗しました。"
+  end
+end
