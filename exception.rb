2995ebd9201f701f3f851b19d7d11f42945b4104
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-01 21:03:14 +0900

    exception add

--- exception.rb
+++ exception.rb
@@ -7,3 +7,22 @@ rescue => e
   puts e.backtrace
   puts "------------------------------------"
 end
+puts
+
+begin
+  puts "abc".foo
+rescue  ZeroDivisionError
+  puts "0で割り算しました"
+  puts "エラークラス:#{e.class}"
+  puts "エラーメッセージ:#{e.message}"
+  puts "----------バックトレース-------------"
+  puts e.backtrace
+  puts "------------------------------------"
+rescue  NoMethodError
+  puts "存在しないメゾットが呼び込まれました"
+  puts "エラークラス:#{e.class}"
+  puts "エラーメッセージ:#{e.message}"
+  puts "----------バックトレース-------------"
+  puts e.backtrace
+  puts "------------------------------------"
+end
