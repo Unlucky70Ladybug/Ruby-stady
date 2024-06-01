602d98a9bb88a5e7edc977d6f3ef66aaca61068d
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-01 20:58:23 +0900

    exception make

---
+++ exception.rb
@@ -0,0 +1,9 @@
+begin
+  puts 1/0
+rescue => e
+  puts "エラークラス:#{e.class}"
+  puts "エラーメッセージ:#{e.message}"
+  puts "----------バックトレース-------------"
+  puts e.backtrace
+  puts "------------------------------------"
+end
