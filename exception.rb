43314338be68f748803194852b3e639ad9ae8ffc
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-02 02:53:25 +0900

    StandardErrorClass use

--- exception.rb
+++ exception.rb
@@ -41,3 +41,33 @@ rescue
     puts "retryに失敗しました。"
   end
 end
+puts
+
+class NoCountryError < StandardError
+  attr_reader :country
+
+  def initialize(message, country)
+    @country = country
+    super("#{message}, #{country}")
+  end
+end
+
+def currency_of(country)
+  case country
+  when :japan
+    "yen"
+  when :us
+    "dollar"
+  when :india
+    "rupee"
+  else
+    raise NoCountryError.new("無効な国名です", country)
+  end
+end
+
+begin
+  currency_of(:italy)
+rescue NoCountryError => e
+  puts e.message
+  puts e.country
+end
