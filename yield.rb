39d81791998b3eb2416e81c1c33dc45cb20cbb27
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-05 00:03:53 +0900

    add

--- yield.rb
+++ yield.rb
@@ -1,12 +1,26 @@
-def greet
+def greet0
   puts "hello"
   if block_given?
     yield
   end
   puts "happy"
 end
-greet
+greet0
+puts
 
-greet do
+greet0 do
   puts "Stady"
+end
+puts
+
+def greet
+  puts "おはよう"
+  text = yield "こんにちは"
+
+  puts text
+  puts "こんばんは"
+end
+
+greet do |text|
+  text * 2
 end
