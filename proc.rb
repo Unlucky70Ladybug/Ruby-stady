957f04b16b1a39511779019fa8558bde6169a1e1
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-05 01:35:51 +0900

    lambda class check

--- proc.rb
+++ proc.rb
@@ -14,11 +14,20 @@ shuffle_proc = Proc.new{|text| text.chars.shuffle.join}
 repeat_proc = Proc.new{|text| text * 2}
 question_proc = Proc.new{|text| "#{text}?"}
 greet(shuffle_proc,repeat_proc,question_proc)
+puts
 
-add_lamda = ->(a,b){a.to_i + b.to_i}
+add_lambda = ->(a,b){a.to_i + b.to_i}
 #ラムダは引数について厳密なので、引数が丁度２つないとエラーが出る
 =begin
 例
-add_lamda.call(10)
-add_lamda.call(10,20,30)
+add_lambda.call(10)
+add_lambda.call(10,20,30)
 =end
+
+add_proc = Proc.new{|a,b| a + b}
+puts add_proc.class
+puts add_proc.lambda?
+
+add_lambda = ->(a,b){a.to_i + b.to_i}
+puts add_lambda.class
+puts add_lambda.lambda?
