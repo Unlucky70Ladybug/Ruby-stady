e1b8b3f262ee7c7b3cfc69213395a03fbc863883
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-05 03:13:27 +0900

    test code add

--- ruby-book/test/word_synth_test.rb
+++ ruby-book/test/word_synth_test.rb
@@ -3,8 +3,8 @@ require_relative "../lib/effects"
 require_relative "../lib/word_synth"
 
 class WordSynthTest < Minitest::Test
-  def test_play
-    assert WordSynth
-    assert Effects
+  def test_play_without_effects
+    synth = WordSynth.new
+    assert_equal "Ruby is fun!", synth.play("Ruby is fun!")
   end
 end
