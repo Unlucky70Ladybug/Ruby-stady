09eea6b51542deb62e8844b422a5ebd568182771
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-06 01:59:17 +0900

    test code add

--- ruby-book/test/log_formatter_test.rb
+++ ruby-book/test/log_formatter_test.rb
@@ -3,6 +3,8 @@ require_relative "../lib/log_formatter"
 
 class LogFomatterTest < Minitest::Test
   def test_format_log
-    LogFomatter.format_log
+    test = LogFomatter.format_log
+    lines = text.lines(chomp: true)
+    assert_equal "[OK] request_id=1, path=/products/1", lines[0]
   end
 end
