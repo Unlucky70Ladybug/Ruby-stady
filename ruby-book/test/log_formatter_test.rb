6706b410ddc51ef4b82917a4bc74c9fa449d7431
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-06 02:58:25 +0900

    test code change

--- ruby-book/test/log_formatter_test.rb
+++ ruby-book/test/log_formatter_test.rb
@@ -4,8 +4,8 @@ require_relative "../lib/log_formatter"
 class LogFomatterTest < Minitest::Test
   def test_format_log
     text = LogFomatter.format_log
+    puts text
     lines = text.lines(chomp: true)
-    pp lines
     assert_equal "[OK] request_id=1, path=/products/1", lines[0]
     assert_equal "[ERROR] request_id=2, path=/wp-login.php, status=404, error=Not found", lines[1]
     assert_equal "[WARN] request_id=3, path=/products, duration=1023.8", lines[2]
