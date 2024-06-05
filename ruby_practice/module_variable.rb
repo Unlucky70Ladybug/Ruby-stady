188a514584c59779a0ee9cf2afacc08729325088
Unlucky70Ladybug <unhappywrld417@gmail.com>
2024-06-06 00:35:33 +0900

    moduleとclassの違い

--- ruby_practice/module_variable.rb
+++ ruby_practice/module_variable.rb
@@ -1,7 +1,7 @@
 module AwesomeApi
   attr_accessor :base_url, :debug_mode
 
-  @base_url = ""
+  @base_url = "https://base/url.com"
   @debug_mode = false
   class << self
     #本来はこの一文のみでよろしい attr_accessor :base_url, :debug_mode
@@ -23,6 +23,39 @@ module AwesomeApi
   end
 end
 
+class ModuleUes
+  attr_reader :name, :age
+  include AwesomeApi
+
+  def initialize(name, age = 20)
+    @name = name
+    @age = age
+  end
+
+  def cahge_age(age)
+    @age = age
+  end
+
+  def puts_base_url
+    AwesomeApi.base_url
+  end
+
+  def update_base_url(new_url)
+    AwesomeApi.base_url = new_url
+  end
+end
+
+test1 = ModuleUes.new("Alice")
+puts test1.name
+puts test1.age
+test1.cahge_age(100)
+puts test1.age
+puts test1.puts_base_url
+puts test1.update_base_url("https://new/url.com")
+puts test1.puts_base_url
+puts "-------------------------------------------------"
+puts AwesomeApi.base_url
+puts "-------------------------------------------------"
 #設定値を保存
 AwesomeApi.base_url = "https://example.com"
 AwesomeApi.debug_mode = true
